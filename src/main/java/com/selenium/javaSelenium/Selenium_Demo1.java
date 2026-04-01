f128512c4804f21a352ceff662c9cf0f5b32a73e
sowndariya <sowndariyadeveloper@gmail.com>
2026-04-01 13:01:40 +0530

    updated edge browser driver

--- src/main/java/com/selenium/javaSelenium/Selenium_Demo1.java
+++ src/main/java/com/selenium/javaSelenium/Selenium_Demo1.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class Selenium_Demo1 {
 	
 	public static void main(String[] args) {
+		
+		 System.setProperty("webdriver.edge.driver", "C:\\Users\\Sowndariya\\Downloads\\edgedriver_win64\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		
