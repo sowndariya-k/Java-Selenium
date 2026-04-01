1968e15b0894289d5843f2338b90107e9fd459b9
sowndariya <sowndariyadeveloper@gmail.com>
2026-04-01 14:00:59 +0530

    update

--- src/test/java/com/selenium/javaSelenium/AppTest.java
+++ src/test/java/com/selenium/javaSelenium/AppTest.java
@@ -1,13 +1,7 @@
 package com.selenium.javaSelenium;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
     
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  }
+  
 }
